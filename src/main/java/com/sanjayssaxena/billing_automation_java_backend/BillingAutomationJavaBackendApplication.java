ce60901ddeaf09941cb0033ad777ba1bb4c0c8e3
sanjaysaxena <sanjaysaxena1094@gmail.com>
2025-09-20 10:12:28 +0530

    Created backend application

---
+++ src/main/java/com/sanjayssaxena/billing_automation_java_backend/BillingAutomationJavaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sanjayssaxena.billing_automation_java_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingAutomationJavaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingAutomationJavaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sanjayssaxena/billing_automation_java_backend/BillingAutomationJavaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanjayssaxena.billing_automation_java_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingAutomationJavaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
